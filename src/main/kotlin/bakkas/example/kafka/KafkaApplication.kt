b91cc13fd3d2154e072714454593be10dcde7d38
BrianDYKim <18k7102dy@naver.com>
2022-07-04 17:37:54 +0900

    Initial commit

---
+++ src/main/kotlin/bakkas/example/kafka/KafkaApplication.kt
@@ -0,0 +1,11 @@
+package bakkas.example.kafka
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KafkaApplication
+
+fun main(args: Array<String>) {
+    runApplication<KafkaApplication>(*args)
+}
